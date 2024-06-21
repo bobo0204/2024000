6bb3fb6607e430fda2a59c09025c04364e3156e0
bobo0204 <156636801+bobo0204@users.noreply.github.com>
2024-06-21 15:02:14 +0800

    Update phonetic.py

--- phonetic.py
+++ phonetic.py
@@ -14,8 +14,8 @@ def read(word):
         phone = [e.text for e in phones]
         s = " ".join( phone )
         # s = row.find('sub')
-        return chinese +' => ' + s 
-    except:
+        word = (f'{chinese} ==> {s})
+        return word
         return  '查無此字' 
     
 #read('國小')
