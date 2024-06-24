962c30fdc9d6a0ec375ae7ed2349cfeb8439e572
11111 <1110211030@gap.wzu.edu.tw>
2024-06-24 16:54:05 +0800

    777

--- app.py
+++ app.py
@@ -1,50 +1,18 @@
-# -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookParser, exceptions
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
 import sys
 import requests
 from bs4 import BeautifulSoup
-import phonetic as ph
-from argparse import ArgumentParser
-from flask import Flask, request, abort
-from linebot import (
-    LineBotApi, WebhookParser
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-from custom_math import custom_math  # 引入 custom_math 函式
+from math_quiz import generate_question, check_answer  # 修改 import 語句
 
 app = Flask(__name__)
 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+# 略過 channel_secret 和 channel_access_token 部分
 
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
-
 
 def get_news():
     url = "https://travel.ettoday.net/category/%E6%A1%83%E5%9C%92/"
@@ -53,7 +21,6 @@ def get_news():
     titles = soup.find_all("h3", itemprop="headline", limit=5)
     news_titles = [title.text.strip() for title in titles]
     return "\n".join(news_titles)
-
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -77,10 +44,11 @@ def callback():
             news = get_news()
             reply_text = news
         elif "題目" in user_message:
-            expression, correct_answer = custom_math()  # 呼叫 custom_math 函式生成題目
+            # 調用 generate_question 函式來生成數學題目
+            expression, correct_answer = generate_question()
             reply_text = f"請計算：{expression}"
         else:
-            reply_text = ph.read(user_message)
+            reply_text = "抱歉，我不太明白你的意思。"
 
         line_bot_api.reply_message(
             event.reply_token,
@@ -89,13 +57,5 @@ def callback():
 
     return 'OK'
 
-
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
+    app.run(debug=True)
